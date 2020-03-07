042941be6497f1327d4b348a4537c067d8944476
idosharon <michdodim@gmail.com>
2020-03-07 10:15:21 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,62 @@
+#  Copyright (c) 2020 Ido Sharon.
+#  This Software created for leechy by Ido Sharon.
+
+import matplotlib.pyplot as plt
+import os
+import pandas as pd
+
+folder = "data/meas/"
+files = os.listdir(folder)
+files.sort()
+
+colors = ["black", "fuchsia", "blue", "sienna", "yellow", "green", "brown", "grey", "orange", "darkorchid", "darkcyan",
+          "red", "#c91414"]
+counter = 0
+
+comparison = []
+const = 1182.69
+
+for file in files:
+
+    file = open(folder + file, "r", encoding="ISO-8859-1")
+    name = file.name[10:].replace(".txt", "").split("_")
+
+    if name[1] == "Store":
+        continue
+
+    print(f"{name[1]} {name[-1]}%")
+
+    data = file.readlines()[17:529]
+    data = [value.replace("\n", "").split("\t") for value in data]
+
+    points_data = [[float(number) for number in value] for value in data]
+
+    print(points_data)
+    x_values = []
+    y_values = []
+
+    [x_values.append(point[0]) for point in points_data]
+    [y_values.append(point[1]) for point in points_data]
+
+    plt.figure(num="leechy | " + folder)
+    plt.plot(x_values, y_values, "--" if name[1] == "water" else "-",
+             label="water" if name[1] == "water" else f"{name[1]} {name[-1]}%", color=colors[counter])
+    plt.ylabel('OD value')
+    plt.xlabel('wave length')
+    plt.legend()
+
+    comparison.append(["water" if name[1] == "water" else f"{name[1]} {name[-1]}%", y_values[x_values.index(const)]])
+
+    counter += 1
+
+
+def last(elem):
+    return elem[-1]
+
+
+comparison = sorted(comparison, key=last)
+comparison = {f'{name[1]} percent: ': [percent[0].replace(name[1], "") for percent in comparison],
+              'OD value: ': [val[-1] for val in comparison]}
+print(pd.DataFrame(data=comparison))
+
+plt.show()
